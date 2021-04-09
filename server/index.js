598ecd9e80eca3bf6a1c72a4f25f5ee93676eb2b
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-09 03:07:19 -0400

    Basic joining room and messaging functionality

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -17,11 +19,25 @@ const io = socketio(server, {
 });
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room});
 
+        if(error) return callback(error);
+
+        socket.join(user.room);
+
+        socket.emit('message', { user: 'Admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'Admin', text: `${user.name}, has joined!`});
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message});
+
+        callback();
     })
 
     socket.on('leave', () => {
