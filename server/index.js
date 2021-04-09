2e28e886b3607b46ec794a514d33f27db365fd25
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-09 16:17:39 -0400

    Implemented chat room functionality and fixed bug with undefined name

--- server/index.js
+++ server/index.js
@@ -18,17 +18,17 @@ const io = socketio(server, {
     }
 });
 
-io.on('connection', (socket) => {
+app.use(cors());
+app.use(router);
+
+io.on('connect', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
         const { error, user } = addUser({ id: socket.id, name, room});
 
         if(error) return callback(error);
 
         socket.join(user.room);
-        console.log(user.room);
 
-        // add check if user.room is undefined or give it a timer
-        console.log(typeof user.room)
         socket.emit('message', { user: 'Admin', text: `${user.name}, welcome to the room ${user.room}`});
         socket.broadcast.to(user.room).emit('message', { user: 'Admin', text: `${user.name}, has joined!`});
 
@@ -37,20 +37,21 @@ io.on('connection', (socket) => {
 
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
-
-        io.to(user.room).emit('message', { user: user.name, text: message});
-
+    
+        io.to(user.room).emit('message', { user: user.name, text: message });
+    
         callback();
-    })
+    });
 
-    socket.on('leave', () => {
-        console.log('User had left');
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        // if(user) {
+        //     io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+        //     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        // }
     })
 });
-
-
-app.use(cors());
-app.use(router);
 
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
