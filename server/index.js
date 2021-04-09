10642bbf2cfdc0e923d833c3db1dc3e7bf47545e
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-09 02:27:06 -0400

    Basic communication between front end and backend
    Shares information about user name and the room name they enter

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,17 +9,30 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+
+const io = socketio(server, {
+    cors: {
+        origin: '*',
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('We have a new connection');
 
-    socket.on('disconnect', () => {
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+
+    })
+
+    socket.on('leave', () => {
         console.log('User had left');
     })
-})
+});
 
+
+app.use(cors());
 app.use(router);
+
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
